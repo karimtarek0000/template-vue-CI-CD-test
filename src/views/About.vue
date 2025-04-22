ec0fe21369516e87b683f5e5322545641c3e463c
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-04-22 17:13:57 +0200

    Fix formatting and correct typo in About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,21 @@
 <script setup>
-import { useHead } from "@vueuse/head";
+import { useHead } from '@vueuse/head'
 
 // Set page-specific metadata
 useHead({
-  title: "About",
-});
+	title: 'About',
+})
 </script>
 
 <template>
-  <div class="about">
-    <h1>About Page</h1>
-    <p>This page is prefetched after the initial load.</p>
-  </div>
+	<div class="about">
+		<h1>About Page</h1>
+		<p>This page issdsd prefetched after the initial load.</p>
+	</div>
 </template>
 
 <style scoped>
 .about {
-  padding: 20px;
+	padding: 20px;
 }
 </style>
