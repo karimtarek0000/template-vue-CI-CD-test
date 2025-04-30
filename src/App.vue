d24eb0574f26baf5758a82a9722c87a3de9e793f
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-04-30 15:19:39 +0300

    fix: add additional content to the template in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,17 @@
 <template>
   <h1>Template CI/CD test</h1>
   <p>Eslint - prettier - unit test</p>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est perspiciatis neque
+    adipisci? Neque, ratione alias officia libero amet dolorem iure voluptatibus dolore fuga sint
+    maxime et ab voluptatum saepe quis id voluptates illo, quia natus? Commodi repudiandae velit,
+    officia ut nam unde inventore laudantium minima nobis accusantium ipsam possimus sequi
+    necessitatibus aliquam nostrum odit dicta cumque dolores blanditiis. Ratione, quaerat
+    doloremque! Dolorem recusandae quia dolores iste consectetur deserunt reiciendis harum
+    praesentium inventore alias non delectus sapiente, aspernatur eligendi ipsum? Harum nam
+    similique officiis dicta voluptatum delectus amet earum, veritatis accusantium fugit magni alias
+    iure, voluptas consectetur laborum consequuntur ex!
+  </p>
 </template>
 
 <style>
